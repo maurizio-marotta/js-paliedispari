1aa7bcee19bb1a20571d673e6c224c750757fc22
maurizio-marotta <mauriziomarotta@live.it>
2023-03-28 16:45:16 +0200

    [inizio]

---
+++ nomi-palindomi/script/script.js
@@ -0,0 +1,9 @@
+/*
+Chiedere all’utente di inserire una parola
+Creare una funzione per capire se la parola inserita è palindroma
+*/
+
+
+let parola = prompt("inserisci la tua parola", "palindroma?");
+
+
