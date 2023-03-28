f04f2131ab353bb2814de26e54092a4c325b6aa5
maurizio-marotta <mauriziomarotta@live.it>
2023-03-28 23:56:35 +0200

    [palindroma finito]

--- nomi-palindomi/script/script.js
+++ nomi-palindomi/script/script.js
@@ -3,7 +3,21 @@ Chiedere all’utente di inserire una parola
 Creare una funzione per capire se la parola inserita è palindroma
 */
 
+var parolaUtente = prompt('inserisci una parola');
+var parolaGirata = rovesciaParola(parolaUtente);
 
-let parola = prompt("inserisci la tua parola", "palindroma?");
+// Creare una funzione per capire se la parola inserita è palindroma
 
+if(parolaGirata == parolaUtente){
+  console.log('è palindroma');
+}else{
+  console.log('non è palindroma');
+}
 
+function rovesciaParola(parola){
+  var parolaReverse = '';
+  for (var i = parola.length -1; i>=0; i--) {
+    parolaReverse += parola[i];
+  }
+  return parolaReverse;
+}
